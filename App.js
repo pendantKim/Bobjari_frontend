1efb7cb3f877a9932129fdcb3daca2b9e1cd8c25
김동한 <pendant@gimdonghan-ui-MacBookPro.local>
2021-02-21 13:49:45 +0900

    Add Tab navigation, screens and Tabs

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { StyleSheet, Text, View } from "react-native";
+
+import TabNavigation from "./navigation/TabNavigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [loaded, setLoaded] = useState(false);
+    const preLoad = async () => {
+        try {
+            setLoaded(true);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    useEffect(() => {
+        preLoad();
+    }, []);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return loaded ? <TabNavigation /> : <AppLoading />;
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Bobjari_frontend
+
+#Pakages
+
+1. styled-components  
+   : `yarn add styled-components`  
+   스타일 컴포넌트 만들어서 사용
+2. React Navigation
+   : `yarn add @react-navigation/native`
+   라우터 역할, 페이지 전환용 네비게이션
